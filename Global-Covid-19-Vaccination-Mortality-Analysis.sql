03cc5271536440ede10d227d120c8bb835e7ea42
WilmerLozano - DataAnalyst <wglozanoh@gmail.com>
2025-01-30 00:42:28 -0500

    Análisis de Mortalidad y Vacunación Global por COVID-19 (2020-2024)

--- Global-Covid-19-Vaccination-Mortality-Analysis.sql
+++ Global-Covid-19-Vaccination-Mortality-Analysis.sql
@@ -1,114 +1,110 @@
+-- "Análisis de Mortalidad y Vacunación Global por COVID-19 (2020-2024)"
 
--- " Análisis de Mortalidad y Vacunación Global por COVID-19 (2020-2024)"
-
---Puedes descargar la data para importar ambas tablas aquí:
---👉covid_deaths: https://github.com/WLozanoH/Global-Covid-19-Vaccination-Mortality-Analysis/blob/main/CovidDeaths.zip
---👉covid_vaccinations: https://github.com/WLozanoH/Global-Covid-19-Vaccination-Mortality-Analysis/blob/main/CovidVaccinations.zip
-
-
+-- ELIMINAMOS LA TABLA SI EXISTE
+-- Esta instrucción elimina cualquier tabla existente con el nombre 'covid_deaths' antes de crearla.
 DROP TABLE IF EXISTS covid_deaths;
---Crea la tabla con los tipos de datos adecuados para covid_deaths
 
+-- CREACIÓN DE LA TABLA 'covid_deaths'
+-- Creamos una tabla para almacenar la información de las muertes por COVID-19 en diferentes países y continentes.
 CREATE TABLE covid_deaths (
-    iso_code VARCHAR(10),
-    continent VARCHAR(50),
-    location VARCHAR(100),
-    date DATE,
-    population BIGINT,
-    total_cases INT,
-    new_cases INT,
-    new_cases_smoothed FLOAT,
-    total_deaths INT,
-    new_deaths INT,
-    new_deaths_smoothed FLOAT,
-    total_cases_per_million FLOAT,
-    new_cases_per_million FLOAT,
-    new_cases_smoothed_per_million FLOAT,
-    total_deaths_per_million FLOAT,
-    new_deaths_per_million FLOAT,
-    new_deaths_smoothed_per_million FLOAT,
-    reproduction_rate FLOAT,
-    icu_patients INT,
-    icu_patients_per_million FLOAT,
-    hosp_patients INT,
-    hosp_patients_per_million FLOAT,
-    weekly_icu_admissions INT,
-    weekly_icu_admissions_per_million FLOAT,
-    weekly_hosp_admissions INT,
-    weekly_hosp_admissions_per_million FLOAT
+    iso_code VARCHAR(10),	-- Código ISO del país
+    continent VARCHAR(50),	-- Continente
+    location VARCHAR(100),	-- Nombre del país o región
+    date DATE,	-- Fecha de la medición
+    population BIGINT,	-- Población del país
+    total_cases INT,	-- Total de casos de COVID-19
+    new_cases INT,	-- Nuevos casos reportados
+    new_cases_smoothed FLOAT,	-- Nuevos casos suavizados
+    total_deaths INT,	-- Total de muertes por COVID-19
+    new_deaths INT,	-- Nuevas muertes reportadas
+    new_deaths_smoothed FLOAT,	-- Nuevas muertes suavizadas
+    total_cases_per_million FLOAT,	-- Total de casos por millón de habitantes
+    new_cases_per_million FLOAT,	-- Nuevos casos por millón de habitantes
+    new_cases_smoothed_per_million FLOAT,	-- Nuevos casos suavizados por millón
+    total_deaths_per_million FLOAT,	-- Total de muertes por millón de habitantes
+    new_deaths_per_million FLOAT,	-- Nuevas muertes por millón de habitantes
+    new_deaths_smoothed_per_million FLOAT,	-- Nuevas muertes suavizadas por millón
+    reproduction_rate FLOAT,	-- Tasa de reproducción del virus
+    icu_patients INT,	-- Número de pacientes en cuidados intensivos
+    icu_patients_per_million FLOAT,	-- Pacientes en cuidados intensivos por millón
+    hosp_patients INT,	-- Número de pacientes hospitalizados
+    hosp_patients_per_million FLOAT,	-- Pacientes hospitalizados por millón
+    weekly_icu_admissions INT,	-- Ingresos semanales en unidades de cuidados intensivos
+    weekly_icu_admissions_per_million FLOAT,	-- Ingresos semanales por millón en unidades de cuidados intensivos
+    weekly_hosp_admissions INT,	-- Ingresos semanales hospitalarios
+    weekly_hosp_admissions_per_million FLOAT	-- Ingresos semanales hospitalarios por millón
 );
 
 	
---Usa el comando COPY para importar los datos desde el archivo CSV.
-	
-COPY covid_deaths FROM 'C:\Users\LENOVO\Desktop\Projects\Covid-19\CovidDeaths.csv' --ruta de donde se extraerá los datos a importar
-DELIMITER ';'
-HEADER CSV;
+-- CARGA DE DATOS EN LA TABLA 'covid_deaths'
+-- Usamos el comando COPY para importar los datos desde un archivo CSV.
+COPY covid_deaths FROM 'C:\Users\LENOVO\Desktop\Projects\Covid-19\CovidDeaths.csv' -- Ruta donde se encuentra el archivo CSV
+DELIMITER ';'	-- El delimitador usado en el archivo CSV es el punto y coma
+HEADER CSV;	-- El archivo CSV tiene una fila de encabezado
 
---Verifica los datos después de la importación.
-SELECT
-	*
-FROM covid_deaths;
+-- VERIFICACIÓN DE LOS DATOS CARGADOS EN 'covid_deaths'
+-- Hacemos una consulta para ver los primeros registros de la tabla y verificar que la importación fue exitosa.
+SELECT * FROM covid_deaths;
 
---Crea la tabla con los tipos de datos adecuados para covid_Vaccinations
-
+-- ELIMINAMOS LA TABLA 'covid_vaccinations' SI EXISTE
 DROP TABLE IF EXISTS covid_vaccinations;
 
+-- CREACIÓN DE LA TABLA 'covid_vaccinations'
+-- Creamos una tabla para almacenar los datos de vacunación contra el COVID-19 a nivel mundial.
 CREATE TABLE covid_vaccinations (
-    iso_code VARCHAR(10),
-    continent VARCHAR(50),
-    location VARCHAR(100),
-    date DATE,
-    total_tests BIGINT,
-    new_tests BIGINT,
-    total_tests_per_thousand FLOAT,
-    new_tests_per_thousand FLOAT,
-    new_tests_smoothed FLOAT,
-    new_tests_smoothed_per_thousand FLOAT,
-    positive_rate FLOAT,
-    tests_per_case FLOAT,
-    tests_units VARCHAR(50),
-    total_vaccinations BIGINT,
-    people_vaccinated BIGINT,
-    people_fully_vaccinated BIGINT,
-    total_boosters BIGINT,
-    new_vaccinations BIGINT,
-    new_vaccinations_smoothed BIGINT,
-    total_vaccinations_per_hundred FLOAT,
-    people_vaccinated_per_hundred FLOAT,
-    people_fully_vaccinated_per_hundred FLOAT,
-    total_boosters_per_hundred FLOAT,
-    new_vaccinations_smoothed_per_million FLOAT,
-    new_people_vaccinated_smoothed BIGINT,
-    new_people_vaccinated_smoothed_per_hundred FLOAT,
-    stringency_index FLOAT,
-    population_density FLOAT,
-    median_age FLOAT,
-    aged_65_older FLOAT,
-    aged_70_older FLOAT,
-    gdp_per_capita FLOAT,
-    extreme_poverty FLOAT,
-    cardiovasc_death_rate FLOAT,
-    diabetes_prevalence FLOAT,
-    female_smokers FLOAT,
-    male_smokers FLOAT,
-    handwashing_facilities FLOAT,
-    hospital_beds_per_thousand FLOAT,
-    life_expectancy FLOAT,
-    human_development_index FLOAT,
-    excess_mortality_cumulative_absolute FLOAT,
-    excess_mortality_cumulative FLOAT,
-    excess_mortality FLOAT,
-    excess_mortality_cumulative_per_million FLOAT
+    iso_code VARCHAR(10),	-- Código ISO del país
+    continent VARCHAR(50),	-- Continente
+    location VARCHAR(100),	-- País o región
+    date DATE,	-- Fecha de la medición
+    total_tests BIGINT,	-- Total de pruebas realizadas
+    new_tests BIGINT, -- Nuevas pruebas realizadas
+    total_tests_per_thousand FLOAT, -- Total de pruebas por mil habitantes
+    new_tests_per_thousand FLOAT, -- Nuevas pruebas por mil habitantes
+    new_tests_smoothed FLOAT, -- Nuevas pruebas suavizadas
+    new_tests_smoothed_per_thousand FLOAT, -- Nuevas pruebas suavizadas por mil habitantes
+    positive_rate FLOAT,	-- Tasa de resultados positivos
+    tests_per_case FLOAT,	-- Número de pruebas por caso
+    tests_units VARCHAR(50),	-- Unidades de prueba
+    total_vaccinations BIGINT,	-- Total de vacunas administradas
+    people_vaccinated BIGINT,	-- Personas vacunadas al menos una vez
+    people_fully_vaccinated BIGINT,	-- Personas completamente vacunadas
+    total_boosters BIGINT,	-- Total de dosis de refuerzo administradas
+    new_vaccinations BIGINT,	-- Nuevas vacunas administradas
+    new_vaccinations_smoothed BIGINT,	-- Nuevas vacunas suavizadas
+    total_vaccinations_per_hundred FLOAT,	-- Total de vacunas por cada 100 habitantes
+    people_vaccinated_per_hundred FLOAT,	-- Personas vacunadas por cada 100 habitantes
+    people_fully_vaccinated_per_hundred FLOAT,	-- Personas completamente vacunadas por cada 100 habitantes
+    total_boosters_per_hundred FLOAT,	-- Dosis de refuerzo por cada 100 habitantes
+    new_vaccinations_smoothed_per_million FLOAT,	-- Nuevas vacunas suavizadas por millón de habitantes
+    new_people_vaccinated_smoothed BIGINT,	-- Nuevas personas vacunadas suavizadas
+    new_people_vaccinated_smoothed_per_hundred FLOAT,	-- Nuevas personas vacunadas por cada 100 habitantes
+    stringency_index FLOAT,	-- Índice de severidad de las medidas de control
+    population_density FLOAT,	-- Densidad poblacional
+    median_age FLOAT,	-- Edad media de la población
+    aged_65_older FLOAT,	-- Porcentaje de personas mayores de 65 años
+    aged_70_older FLOAT,	-- Porcentaje de personas mayores de 70 años
+    gdp_per_capita FLOAT,	-- Producto interno bruto per cápita
+    extreme_poverty FLOAT,	-- Porcentaje de población en extrema pobreza
+    cardiovasc_death_rate FLOAT,	-- Tasa de muertes por enfermedades cardiovasculares
+    diabetes_prevalence FLOAT,	-- Prevalencia de diabetes
+    female_smokers FLOAT,	-- Porcentaje de mujeres fumadoras
+    male_smokers FLOAT,	-- Porcentaje de hombres fumadores
+    handwashing_facilities FLOAT,	-- Porcentaje de población con acceso a instalaciones para lavarse las manos
+    hospital_beds_per_thousand FLOAT,	-- Número de camas hospitalarias por mil habitantes
+    life_expectancy FLOAT,	-- Esperanza de vida
+    human_development_index FLOAT,	-- Índice de desarrollo humano
+    excess_mortality_cumulative_absolute FLOAT,	-- Mortalidad excedente acumulada absoluta
+    excess_mortality_cumulative FLOAT,	-- Mortalidad excedente acumulada
+    excess_mortality FLOAT,	-- Mortalidad excedente
+    excess_mortality_cumulative_per_million FLOAT	-- Mortalidad excedente acumulada por millón de habitantes
 );
 
---Usa el comando COPY para importar los datos desde el archivo CSV.
-	
+-- CARGA DE DATOS EN LA TABLA 'covid_vaccinations'
 COPY covid_vaccinations FROM 'C:\Users\LENOVO\Desktop\Projects\Covid-19\CovidVaccinations.csv' --ruta de donde se extraerá los datos a importar
-DELIMITER ';'
-HEADER CSV;
+DELIMITER ';'	-- Delimitador en el archivo CSV
+HEADER CSV;		-- El archivo tiene una fila de encabezado
 
---Verifica los datos después de la importación.
+-- VERIFICACIÓN DE LOS DATOS CARGADOS EN 'covid_vaccinations'
 SELECT
 	*
 FROM covid_vaccinations;
@@ -125,18 +121,25 @@ SELECT
 FROM covid_deaths
 WHERE continent IS NOT NULL
 order by 3,4;
---
 
--- Seleccionamos la data a usar
--- Selecting data to use
+-- Verificar si hay registros con datos faltantes o nulos en las columnas clave
+SELECT * FROM covid_deaths WHERE total_cases IS NULL OR total_deaths IS NULL;
+
+-- Reemplazar los valores nulos con 0
+UPDATE covid_deaths
+SET total_cases = COALESCE(total_cases, 0),
+    total_deaths = COALESCE(total_deaths, 0);
+
+-- SELECCIONAMOS LOS DATOS DE INTERÉS
+-- Extraemos una muestra representativa de los datos de casos, muertes y población de 'covid_deaths'.
 SELECT
 	location, date, population, total_cases, new_cases, total_deaths
 FROM covid_deaths
 WHERE continent IS NOT NULL
 ORDER BY 1,2,3;
 
---Observamos 'total_cases' vs 'total_deaths'
---Obtenemos el porcentaje de muertes respecto a la totalidad de contagios
+-- CALCULAMOS EL PORCENTAJE DE MUERTES RESPECTO A LOS CASOS
+-- Comparamos las muertes totales con los casos totales, calculando el porcentaje de muertes por cada país.
 SELECT
 	location, date, total_cases, total_deaths,
 	ROUND((total_deaths::NUMERIC/NULLIF(total_cases,0)),2)*100 AS death_percentage
@@ -146,8 +149,8 @@ AND total_cases IS NOT NULL
 AND total_cases <> 0
 ORDER BY 1,2;
 
--- Obtenemos el porcentaje de muerte mas alto por país 
---Análisis sobre 10 países durante '2020-04-01' / '2021-06-30'
+-- SELECCIONAMOS LOS PAÍSES CON EL PORCENTAJE DE MUERTES MÁS ALTO
+-- Analizamos el porcentaje máximo de muertes por país en el periodo entre 2020-04-01 y 2021-06-30.
 SELECT
 	location,
 	MAX(death_percentage) AS max_death_percentage_per_country
@@ -165,23 +168,15 @@ FROM( SELECT
 ) AS Percentage_deaths_countries
 GROUP BY location
 ORDER BY location;
-/* 
-Porcentaje más alto:
-- "United Kingdom" - 23%
-Porcentaje más bajo:
--"New Zealand"- 2%
-Porcentaje de muerte en Perú
--"Peru" - 14%
-*/
 
--- Paises con la infeccion más alta respecto a su población 
--- Countries with highest infection per population
+-- ANALIZAMOS LOS PAÍSES CON LA INFECCIÓN MÁS ALTA POR POBLACIÓN
+-- Analizamos qué países tienen la mayor tasa de infección más alta
 SELECT
 	location,
 	MAX(percentPopulationInfected) AS country_infected
 	FROM( SELECT
 		location, date, total_cases, population, 
-		ROUND((total_cases::NUMERIC/population),2) *100 AS percentPopulationInfected
+		ROUND((total_cases::NUMERIC / population),2) *100 AS percentPopulationInfected
 	FROM covid_deaths
 	WHERE continent IS NOT NULL
 	AND total_cases IS NOT NULL
@@ -193,14 +188,6 @@ SELECT
 	) AS countries_infected
 GROUP BY location
 ORDER BY country_infected DESC;
-/* Tasa de contagio respecto a su población
-
-País con mayor tasa de contagio:
-- Estados unidos: 10%
-País con menor tasa de contagio:
--Nueva Zelanda: 0%
-Perú: 6%
-*/
 
 --Mostrando los países con la cantidad de muertes más alta por poblacion 
 --Showing countries with highest death count per population
@@ -245,7 +232,7 @@ ORDER BY Max_deaths_per_continent DESC;
 SELECT
 	SUM(new_cases) AS total_new_cases,
 	SUM(new_deaths) AS total_new_deaths,
-	(SUM(new_deaths::NUMERIC)/SUM(new_cases::NUMERIC))*100 AS deathsNewPercentage
+	ROUND((SUM(new_deaths::NUMERIC)/SUM(new_cases::NUMERIC)*100),2) AS deathsNewPercentage
 FROM covid_deaths
 WHERE continent IS NOT NULL
 AND new_cases IS NOT NULL
@@ -360,7 +347,7 @@ vaccinations_percentage_habitants NUMERIC
 
 --COPIAMOS LOS DATOS ANALIZADOS A LA NUEVA TABLA 'PercentagePopulationVaccinated'
 COPY PercentagePopulationVaccinated 
-FROM 'C:\Users\LENOVO\Desktop\Projects\Covid-19\PercentagePopulationVaccinated.csv'
+FROM 'C:\Users\LENOVO\Desktop\Projects\Covid-19\PercentagePopulationVaccinated.csv' --ruta donde se encuentra el archivo
 DELIMITER ','
 CSV HEADER;
 
